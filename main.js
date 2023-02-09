659fe7b37652676a92a1dfcc46f0ac2dfc81f096
BondarAndrew <107639998+BondarAndrew@users.noreply.github.com>
2023-02-09 21:15:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let a = prompt();
+let b = prompt();
+let n1 = Number(a);
+let n2 = Number(b);
+let r = n2 + n1;
+if (r > 10) {
+  alert("okay");
+}
+
+let name = prompt("Enter your name: ");
+
+if (name === "Andrew") {
+  console.log("Hello");
+} else {
+  console.log("Who are you?");
+}
